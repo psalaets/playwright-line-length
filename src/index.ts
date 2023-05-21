1510a1332e145eb35905b9e521b66638ba7252ca
Paul Salaets <psalaets@gmail.com>
2023-05-21 14:50:41 -0500

    Making a mess

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { URL } from 'url';
-import { median } from './math';
 import { elementLineLength as elementLineLengthFn } from "element-line-length";
 import { Locator } from "@playwright/test";
 
@@ -87,4 +86,17 @@ function result(message: string, pass: boolean) {
     message: () => message,
     pass,
   };
+}
+
+function median(values: Array<number>): number | null {
+  if (values.length > 0) {
+    const sortedValues = values.slice().sort((a, b) => a - b);
+    const middleIndex = Math.floor(sortedValues.length / 2);
+
+    return sortedValues.length % 2 === 0
+      ? (sortedValues[middleIndex - 1] + sortedValues[middleIndex]) / 2
+      : sortedValues[middleIndex];
+  } else {
+    return null;
+  }
 }
